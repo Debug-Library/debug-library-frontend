f43a5ee075eabe40fac833963391dc6764d5a750
Maria de Fátima <mnunesalves334@gmail.com>
2025-04-29 16:57:51 -0300

    chore: arquivo de configuração do tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+    theme: {
+      extend: {
+        fontFamily: {
+          montserrat: ['Montserrat', 'sans-serif'],
+        },
+      },
+    },
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  }
+  
