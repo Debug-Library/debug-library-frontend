20ae8c7fde9abed75963be0679091ff5b6294fe2
Maria de Fátima <mnunesalves334@gmail.com>
2025-05-12 13:42:50 -0300

    feat: Adicionar rota principal da homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login/Login";
 import Cadastro from "./components/Cadastro/Cadastro";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path ="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
       </Routes>
