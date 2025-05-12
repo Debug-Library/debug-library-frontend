083a1762f4ebbcfe49ea198810e6cd62955231bc
Maria de Fátima <mnunesalves334@gmail.com>
2025-05-12 13:48:29 -0300

    feat: Adicionar o componente Home

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,11 @@
+import Banner from "../Banner/Banner";
+
+export function Home () {
+    return (
+        <div className="bg-purple-950 min-h-screen">
+            <Banner />
+        </div>
+    );
+}
+
+export default Home;
