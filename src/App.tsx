22830ba4cab5becaf9bd417ebef082eb0a15d487
Maria de Fátima <mnunesalves334@gmail.com>
2025-05-16 17:14:41 -0300

    feat: Adicionar rota para tela de usuário autenticado

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login/Login";
 import Cadastro from "./components/Cadastro/Cadastro";
 import Home from "./components/Home/Home";
+import InformacoesPerfil from "./components/InformacoesPerfil/InformacoesPerfil";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/perfil" element={<InformacoesPerfil />} />
       </Routes>
     </BrowserRouter>
   );
