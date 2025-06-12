d86437bd51a2dbb63b13eccc34db8cc551b06095
Maria de Fátima <mnunesalves334@gmail.com>
2025-06-12 15:10:26 -0300

    chore: resolvendo problemas de conflito de merge

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Banner from "../Banner/Banner";
 import Navbar from "../Navbar/Navbar";
-import BookSliderSection from "../BookSliderSection/BookSliderSection"
+import BookSliderSection from "../BookSliderSection/BookSliderSection";
 import Footer from "../Footer/Footer";
 
 export function Home() {
@@ -16,16 +16,13 @@ export function Home() {
   };
 
   return (
-    <div className="bg-purple-950 min-h-screen">
-      <Navbar user={user} onLogout={handleLogout} />      
-      <BookSliderSection/>
-      <Banner />
     <div className="flex flex-col min-h-screen bg-purple-950">
       {/* Cabeçalho */}
       <Navbar user={user} onLogout={handleLogout} />
 
       {/* Conteúdo principal (cresce para empurrar o footer) */}
       <main className="flex-grow">
+        <BookSliderSection />
         <Banner />
       </main>
 
